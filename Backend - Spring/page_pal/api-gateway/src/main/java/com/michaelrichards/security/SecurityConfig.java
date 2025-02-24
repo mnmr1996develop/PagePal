f6b477070669928b3fdc044e8d04d14ad3d1e431
mnmr1996 <mnmr1996develop@gmail.com>
2025-02-24 14:26:30 -0500

    adding the api-gateway

---
+++ Backend - Spring/page_pal/api-gateway/src/main/java/com/michaelrichards/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.michaelrichards.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@Configuration
+@EnableWebFlux
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+            return httpSecurity.csrf(
+                    Customizer.withDefaults()
+            )
+                    .authorizeExchange( authorizeExchangeSpec ->
+                            authorizeExchangeSpec
+                                    .pathMatchers("(/eureka/**")
+                                    .permitAll()
+                                    .anyExchange()
+                                    .authenticated()
+                    )
+                    .oauth2ResourceServer(
+                            oAuth2ResourceServerSpec ->
+                                    oAuth2ResourceServerSpec.jwt(Customizer.withDefaults())
+                    ).build();
+    }
+
+}
