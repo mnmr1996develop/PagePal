4135135a0f01741f33102d95407e955764d1331a
mnmr1996 <mnmr1996develop@gmail.com>
2025-03-04 13:36:58 -0500

    fixing an issue in the config and discovery server

--- Backend - Spring/page_pal/dicovery-server/src/main/java/com/michaelrichards/DiscoveryServer.java
+++ Backend - Spring/page_pal/dicovery-server/src/main/java/com/michaelrichards/DiscoveryServer.java
@@ -1,12 +1,11 @@
 package com.michaelrichards;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServer {
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServer.class, args);
